56ca7ab308041a78d61a439bcfbb8c9c24cd276a
Lakshan-Ram <lm14358@C1MNJ26PDV30.local>
2018-10-06 12:50:36 +0100

    Modified program to accept arguments from the command line

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -8,11 +8,63 @@ int main(int argc, char* argv[])
 {
 
   const std::vector<std::string> cmdLineArgs{argv, argv+argc};
-  
-  for (int i=0;i<argc;i++)
-    {
+  std::string Input_File{""};
+  std::string Output_File{""};
+  bool encrypt{true};
+  bool ERROR{false};
+  for (int i=1;i<argc;i++) // Starts from the arguments and not the name of the program.
+    { 
       std::cout<<"Argument " << i << ": " << cmdLineArgs[i]<<std::endl;
+      
+      if (cmdLineArgs[i]=="-h" || cmdLineArgs[i]=="--help")
+	{
+	  std::cout<<"This Program encrypts or decrypts the input data. The program is not case-sensitive and only supports Alpha-numeric characters."<<std::endl;
+	  std::cout<<"The Following arguments are required for the program to run"<<std::endl;
+	  std::cout<<"Input FileName, Output FileName, To Encrypt/Decrypt(Default:Encrypt)"<<std::endl; 
+	  //std::cout<<"-i [FileName]:  The name of the input file containing the data to be encrypted/decrypted"<<std::endl;
+	  //std::cout<<"-o [FileName]:  The name of the output file containing the encrypted/decrypted data"<<std::endl;
+	  //std::cout<<"-c [Cipher]  :  The Cipher to be used to encrypt/decrypt the data. The only allowed arguments are ceaser
+	}
+
+      else if (cmdLineArgs[i]=="--encrypt") 
+	{
+	  encrypt = true;
+	}
+
+
+      else if (cmdLineArgs[i]=="--decrypt") 
+	{
+	  encrypt = false;
+	}
+
+      else if (cmdLineArgs[i]=="-i")
+	{
+	  i++;                  // Takes us to the next argument and make s sure the for loop skips over it.
+	  Input_File+=cmdLineArgs[i]; // The imput file name is updated.
+	}
+    
+      else if (cmdLineArgs[i]=="-o")
+	{
+	  i++;
+	  Output_File+=cmdLineArgs[i];
+	}
+
+      else
+	{
+	  std::cout<<"ERROR: Check input argument "<< i << "\n Allowed input options are -h, --help, -i [FileName], -o [FileName], --encrypt, --decrypt"<<std::endl;
+	  ERROR=true;
+	  break;
+	}
     }
+  if (ERROR==false)
+  {
+
+    std::cout<< "Selected Input File: "<< Input_File <<"\nSelected Output File: "<< Output_File<<std::endl;
+    if (encrypt == true)
+      {std::cout<<"Encryption is activated"<<std::endl;}
+    else if (encrypt == false)
+      {std::cout<<"Decryption is activated"<<std::endl;}
+  
   char in_char('x');
   char test_char('x');
   std::string Value {""};
@@ -96,7 +148,7 @@ int main(int argc, char* argv[])
 	}
     }
   std::cout<<Value<<std::endl;
-  
+  }
 }
 
 
