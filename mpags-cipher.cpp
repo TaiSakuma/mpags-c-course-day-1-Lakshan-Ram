e2b714ca136a907848e1709d2201f25dfd64ea16
Lakshan-Ram <lm14358@C1MNJ26PDV30.local>
2018-10-06 10:52:44 +0100

    Input arguments are read and printed

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,93 +1,104 @@
 #include <iostream>
 #include <string>
 #include <ctype.h>
+#include <vector>
+using namespace std;
 
-
-int main()
+int main(int argc, char* argv[])
 {
 
- char in_char('x');
- char test_char('x');
- std::string Value {""};
- std::cout<<"Enter the charcters. When finished press ENTER and Ctrl + D"<<std::endl;
+  const std::vector<std::string> cmdLineArgs{argv, argv+argc};
+  
+  for (int i=0;i<argc;i++)
+    {
+      std::cout<<"Argument " << i << ": " << cmdLineArgs[i]<<std::endl;
+    }
+  char in_char('x');
+  char test_char('x');
+  std::string Value {""};
+  std::cout<<"Enter the charcters to be encryted. When finished press ENTER and Ctrl + D"<<std::endl;
 
   while (std::cin>>in_char)
-  {
-    test_char=in_char;
-    in_char = toupper(in_char);
-
-    switch(in_char)
-      {
-      case '1':
-	Value += 'O';
-	Value += 'N';
-	Value += 'E';
+    {
+      test_char=in_char;
+      in_char = toupper(in_char);
+      
+      switch(in_char)
+	{
+	case '1':
+	  Value += 'O';
+	  Value += 'N';
+	  Value += 'E';
 	break;
-
-      case '2':
-	Value += 'T';
-	Value += 'W';
-	Value += 'O';
+	
+	case '2':
+	  Value += 'T';
+	  Value += 'W';
+	  Value += 'O';
 	break;
-
-      case '3':
-	Value += 'T';
-	Value += 'H';
-	Value += 'R';
-	Value += 'E';
-	Value += 'E';
-      break;
-
-      case '4':
-	Value += 'F';
-	Value += 'O';
-	Value += 'U';
-	Value += 'R';
-	break; 
-     
-      case '5':
-	Value += 'F';
-	Value += 'I';
-	Value += 'V';
-	Value += 'E';
+	
+	case '3':
+	  Value += 'T';
+	  Value += 'H';
+	  Value += 'R';
+	  Value += 'E';
+	  Value += 'E';
 	break;
-
-      case '6':
-	Value += 'S';
-	Value += 'I';
-	Value += 'X';
-	break;
-
-      case '7':
-	Value += 'S';
-	Value += 'E';
-	Value += 'V';
-	Value += 'E';
-	Value += 'N';
-	break;
-
-      case '8':
-	Value += "EIGHT";
-	break;
-
-      case '9':
-	Value += "NINE";
-      break;
-
-      case '0':
-	Value += "ZERO";
-	break;
-      default:
-	if (in_char != test_char)
-	  {
-	    Value += in_char;
-	  }
-	else
-	  {break;}
-
-      break;
-      }
-  }
+	
+	case '4':
+	  Value += 'F';
+	  Value += 'O';
+	  Value += 'U';
+	  Value += 'R';
+	  break; 
+	  
+	case '5':
+	  Value += 'F';
+	  Value += 'I';
+	  Value += 'V';
+	  Value += 'E';
+	  break;
+	  
+	case '6':
+	  Value += 'S';
+	  Value += 'I';
+	  Value += 'X';
+	  break;
+	  
+	case '7':
+	  Value += 'S';
+	  Value += 'E';
+	  Value += 'V';
+	  Value += 'E';
+	  Value += 'N';
+	  break;
+	  
+	case '8':
+	  Value += "EIGHT";
+	  break;
+	  
+	case '9':
+	  Value += "NINE";
+	  break;
+	  
+	case '0':
+	  Value += "ZERO";
+	  break;
+	default:
+	  if (in_char != test_char)
+	    {
+	      Value += in_char;
+	    }
+	  else
+	    {break;}
+	  
+	  break;
+	}
+    }
   std::cout<<Value<<std::endl;
-
+  
 }
+
+
+
+
